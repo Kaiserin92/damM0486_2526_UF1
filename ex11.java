120efcad99511a2b18c9120a73f546b8b2bb432b
Gisela Ruiz <giselarf1992@outlook.com>
2025-09-22 17:36:04 +0200

    lliurament_ex11_22_09_25

---
+++ ex11.java
@@ -0,0 +1,44 @@
+import java.io.File;
+
+public class ex11 {
+    public static void main(String[] args) {
+
+        String ruta = args[0];
+        File path = new File(ruta);
+
+        File[] files = path.listFiles();
+
+        for (File file : files) {
+            StringBuilder permisos = new StringBuilder();
+
+            if (file.isDirectory()) {
+                permisos.append('d');
+            } else if (file.isFile()) {
+                permisos.append('-');
+            } else {
+                // Entradas no clasificables como archivo/directorio
+                permisos.append('?');
+            }
+
+            if (file.canRead()) {
+                permisos.append('r');
+            } else {
+                permisos.append('-');
+            }
+
+            if (file.canWrite()) {
+                permisos.append('w');
+            } else {
+                permisos.append('-');
+            }
+
+            if (file.canExecute()) {
+                permisos.append('x');
+            } else {
+                permisos.append('-');
+            }
+
+            System.out.println(permisos + " " + file.getName());
+        }
+    }
+}
